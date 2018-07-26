7d0051f5a1da09623a83dfcb1622a7b17877abfb
Aidan M <asmaidan@outlook.com>
2018-07-25 22:33:25 -0500

    Java platform Channel

--- lib/saturnpackage.dart
+++ lib/saturnpackage.dart
@@ -6,14 +6,23 @@ class Saturnpackage {
   static const MethodChannel _channel =
       const MethodChannel('saturnpackage');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion', null);
-    return version;
-  }
-
+  // Build and submit a TX to the stellar network
   static Future<String>  submitStellarTx(String destination, String source, String amount) async {
     final String tx = await _channel.invokeMethod('submit_stellar_tx', {"destination":destination, "source":source, "amount":amount});
     return tx;
   }
+
+  // Generate a Stellar KeyPair
+  static Future<String> stellarKeyPairSeed(dynamic seed) async {
+    final String keyPair = await _channel.invokeMethod('stellar_keyPair_seed', {"seed": seed});
+    return keyPair;
+  }
+
+  // Create a Stellar account with FriendBot
+  static Future<String> stellarCreateTestAccount(dynamic accountPair) async {
+    final String result = await _channel.invokeMethod('stellar_create_test_account', {'accountpair': accountPair});
+    return result;
+  }
+
 
 }
