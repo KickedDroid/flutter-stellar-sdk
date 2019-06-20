96e8a534bb24397743ff6e35e50674580b2f307a
Aidan M <asmaidan@outlook.com>
2019-06-20 16:19:36 -0500

    Update packages

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,33 +12,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await Saturnpackage.stellarKeyPairSeed('aaa');
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +21,10 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: new Center(
-          child: new Text('Running on: $_platformVersion\n'),
+          child: new FlatButton(
+            onPressed: () => Saturnpackage.stellarKeyPairSeed('seed'),
+            child: new Text('Seed'),
+          ),
         ),
       ),
     );
--- lib/saturnpackage.dart
+++ lib/saturnpackage.dart
@@ -9,18 +9,21 @@ class Saturnpackage {
   // Build and submit a TX to the stellar network
   static Future<String>  submitStellarTx(String destination, String source, String amount) async {
     final String tx = await _channel.invokeMethod('submit_stellar_tx', {"destination":destination, "source":source, "amount":amount});
+    print(tx);
     return tx;
   }
 
   // Generate a Stellar KeyPair
-  static Future<String> stellarKeyPairSeed(dynamic seed) async {
+  static Future<String> stellarKeyPairSeed(String seed) async {
     final String keyPair = await _channel.invokeMethod('stellar_keyPair_seed', {"seed": seed});
+    print(keyPair);
     return keyPair;
   }
 
   // Create a Stellar account with FriendBot
-  static Future<String> stellarCreateTestAccount(dynamic accountPair) async {
+  static Future<String> stellarCreateTestAccount(String accountPair) async {
     final String result = await _channel.invokeMethod('stellar_create_test_account', {'accountpair': accountPair});
+    print(result);
     return result;
   }
 
